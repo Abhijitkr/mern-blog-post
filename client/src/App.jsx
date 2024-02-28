d883e5f0b223d8fdbe2568ea81beec3998160e90
AbhijitKr <abhijitkr600@gmail.com>
2024-02-28 23:36:47 +0530

    Adds Routing to the navlinks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import AddBlog from "./pages/AddBlog";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <main className="container mx-auto bg-green-200 p-5">
+    <main className="container mx-auto p-5">
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/add-blog" element={<AddBlog />} />
+      </Routes>
     </main>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <section>
       <div className="flex justify-between">
-        <h1 className="text-3xl font-semibold cursor-pointer">MERN Blog</h1>
+        <h1 className="text-3xl font-semibold cursor-pointer">
+          <Link to="/">MERN Blog</Link>
+        </h1>
         <ul className="flex items-center gap-20 text-xl font-medium cursor-pointer">
-          <li>Home</li>
-          <li>Add Blog</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/add-blog">Add Blog</Link>
+          </li>
         </ul>
       </div>
     </section>
