dba5c5ae033d3147dfe3140a9dadabaebdac24ca
AbhijitKr <abhijitkr600@gmail.com>
2024-03-01 23:23:53 +0530

    Successfully Adds blog to the Database

--- client/src/context/GlobalContext.jsx
+++ client/src/context/GlobalContext.jsx
@@ -1,9 +1,15 @@
-import { createContext } from "react";
-
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}){
-    const test="testing";
-    return <GlobalContext.Provider value={{test}}>{children}</GlobalContext.Provider>
-} 
+export default function GlobalState({ children }) {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  return (
+    <GlobalContext.Provider value={{ formData, setFormData }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
--- client/src/pages/AddBlog.jsx
+++ client/src/pages/AddBlog.jsx
@@ -1,7 +1,30 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { GlobalContext } from "../context/GlobalContext";
+
 export default function AddBlog() {
+  const { formData, setFormData } = useContext(GlobalContext);
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8000/api/blogs/add", {
+      method: "POST",
+      headers: { "Content-Type": "application.json" },
+      body: JSON.stringify(formData),
+    });
+    if (response.ok) {
+      setFormData({
+        title: "",
+        description: "",
+      });
+    }
+    navigate("/");
+  }
+
   return (
     <section>
-      <h1 className="text-2xl font-semibold">Add Blog</h1>
+      <h1 className="text-2xl font-semibold text-center">Add Blog</h1>
       <div className="flex flex-col p-10 w-[800px] gap-5 mx-auto">
         <input
           type="text"
@@ -9,6 +32,8 @@ export default function AddBlog() {
           id="title"
           placeholder="Enter blog title"
           className="p-2 border border-gray-300 outline-gray-400"
+          value={formData.title}
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
         />
         <textarea
           name="description"
@@ -17,10 +42,15 @@ export default function AddBlog() {
           rows="10"
           placeholder="Enter blog description"
           className="p-2 border border-gray-300 outline-gray-400"
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
+          value={formData.description}
         ></textarea>
         <button
-          type="submit"
+          // type="submit"
           className="p-2 border border-gray-300 outline-gray-400 transition-transform duration-200 transform hover:scale-105 active:scale-95"
+          onClick={(e) => handleSubmit(e)}
         >
           Add the Blog
         </button>
