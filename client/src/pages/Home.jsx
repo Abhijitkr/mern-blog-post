43daee2395c6024c0bbce89ca2d4391195158904
AbhijitKr <abhijitkr600@gmail.com>
2024-03-06 16:10:26 +0530

    Add fetch api to fetch blog list from the database

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,42 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
 export default function Home() {
   const { blogList, setBlogList, pending, setPending } =
     useContext(GlobalContext);
 
-  // useEffect(() => {
-  //   (async function fetchBlogPost() {
-  //     const response = await fetch("http://localhost:8000/api/blogs");
-  //     const data = await response.json();
-  //     if (data && data.blogs && data.blogs.length) {
-  //       setBlogList(data.blogs);
-  //     }
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async function fetchBlogPost() {
+      setPending(true);
+      try {
+        const response = await fetch("http://localhost:8000/api/blogs");
+        const data = await response.json();
+        if (data && data.blogList) {
+          setBlogList(data.blogList);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setPending(false);
+      }
+    })();
+  }, []);
 
-  return <section>Home</section>;
+  return pending ? (
+    <div>Loading....</div>
+  ) : (
+    <section className="flex gap-10 justify-center">
+      {blogList &&
+        blogList.length &&
+        blogList.map((blog) => (
+          <div
+            key={blog._id}
+            className="border border-yellow-300 w-80 text-center p-3"
+          >
+            <h2 className="text-xl font-semibold">{blog.title}</h2>
+            <p>{blog.description}</p>
+          </div>
+        ))}
+    </section>
+  );
 }
